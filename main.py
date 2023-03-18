556a0e1704f0b2ad5742f9e32922028d383aea24
SumedhaKun <kundurthisumedha@gmail.com>
2023-03-18 10:46:52 -0700

    next commit

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+
+class Vybe(App):
+    
+    def build(self):
+        self.window=GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5 }
+        return self.window
+
+if __name__ == "__main__":
+ Vybe().run()
