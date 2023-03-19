2f53e353eb49a0c9568b70d1206f30f9712ececb
SumedhaKun <kundurthisumedha@gmail.com>
2023-03-18 17:00:19 -0700

    next

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from kivy.app import App
-# GridLayout arranges children in a matrix.
 from kivy.uix.gridlayout import GridLayout
-# Label is used to label something
 from kivy.uix.label import Label
-# used to take input from users
 from kivy.uix.textinput import TextInput
 
 
@@ -13,10 +10,11 @@ class Vybe(GridLayout):
     def __init__(self, **var_args):
  
         super(Vybe, self).__init__(**var_args)
-        self.cols = 1     # You can change it accordingly
+        self.cols = 1     
         self.add_widget(Label(text='What is your mood?'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
+        self.mood = TextInput(multiline=False)
+        self.add_widget(self.mood)
+        self.list=[]
         
 class MyApp(App):
     def build(self):
@@ -25,6 +23,7 @@ class MyApp(App):
  
 if __name__ == '__main__':
     MyApp().run()
+
 
 
 
